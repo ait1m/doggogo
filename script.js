fc211209591206e03ebf357d53097a1d92c14afc
ait1m <68070042@kmitl.ac.th>
2025-10-03 10:37:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const myinput = document.getElementById("myinput")
+
+function add() {
+    myinput.value = Number(myinput.value) + 1
+}
+function low() {
+    myinput.value = Number(myinput.value) - 1
+}
